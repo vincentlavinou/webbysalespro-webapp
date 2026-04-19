ac2f09006033c126ee6ef677aadcaf94365cca77
Vincent Turnier <vincentturnier@gmail.com>
2026-04-19 16:55:07 -0400

    updated video for better experience

--- src/playback/stage/AttendeeStageViewer.tsx
+++ src/playback/stage/AttendeeStageViewer.tsx
@@ -74,7 +74,7 @@ export const AttendeeStageViewer = forwardRef<
     videoRef,
     containerRef: playerSurfaceRef,
     onResumeNeeded: () => {
-      void reconnectStage({ gracePeriodMs: 150 });
+      void reconnectStage();
     },
   });
 
